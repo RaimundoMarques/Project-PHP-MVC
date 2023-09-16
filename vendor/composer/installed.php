e1d07ee2ebceea62b405ff17de56316b19c26804
raimundo.marques <raimundo.marques.ff@gmail.com>
2023-09-15 21:21:36 -0400

    update code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0aa916cdbd78796d397d337aa15025429fff97e7',
+        'name' => 'project/mvc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'project/mvc' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0aa916cdbd78796d397d337aa15025429fff97e7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
