f0e2d873aac0932e67fd1cd6bc3c1e7faaab7ebd
raimundo.marques <raimundo.marques.ff@gmail.com>
2023-10-16 22:08:25 -0400

    add method router

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -2,8 +2,10 @@
 
 namespace App\Http;
 
+use \Closure;
 
-class Router{
+class Router
+{
 
     /**
      * URL completa do projeto (raiz)
@@ -40,5 +42,63 @@ class Router{
     {
         $this->request = new Request();
         $this->url = $url;
+        $this->setPrefix();
+    }
+
+    /**
+     * Método responsável por definir o prefixo das rotas
+     * 
+     */
+    private function setPrefix()
+    {
+        // Informações da URL ATUAL
+        $parseURL = parse_url($this->url);
+
+        // Define prefixo 
+        $this->prefix = $parseURL['path'] ?? '';
+    }
+
+
+    /**
+     * Método responsável por adicionar uma rota na clase
+     * @param string $method
+     * @param string $route
+     * @param array $params
+     */
+    private function addRoute($method, $route, $params = [])
+    {
+        // Validação dos parâmetros
+        foreach ($params as $key => $value) {
+            if ($value instanceof Closure) {
+                $params['controller'] = $value;
+                unset($params['$key']);
+                continue;
+            }
+        }
+
+        // Padrão de validação da URL
+        $pattenernRoute = '/^' . str_replace('/', '\/', $route) . '$/';
+
+        // Adicionando rota dentro da classe
+        $this->routes[$pattenernRoute][$method] = $params;
+        echo "<pre>";
+        print_r($this);
+        echo "</pre>";
+        exit;
+    }
+
+    /**
+     * Método responsável por definir uma rota de GET
+     * @param string $route
+     * @param array $params
+     */
+    public function get($route, $params = [])
+    {
+        return $this->addRoute('GET', $route, $params);
     }
 }
+
+        // echo "<pre>";
+        // print_r($pattenernRoute);
+        // echo "</pre>";
+        // exit;
--- index.php
+++ index.php
@@ -4,16 +4,22 @@
 require __DIR__ . "/vendor/autoload.php";
 
 use \App\Http\Router;
+use \App\Http\Response;
 use \App\Controller\Pages\Home;
 
-define('URL', 'http://localhost/project-php-mvc/');
+define('URL', 'http://localhost/project-php-mvc');
 
 $obrouter = new Router(URL);
 
-echo "<pre>";
-print_r($obrouter);
-echo "</pre>";
 
-exit;
+// Definindo a rota da página HOME
+$obrouter->get('/', [
+    function () {
+        return new Response(200, Home::getHome());
+    }
+]);
 
-echo Home::getHome();
+
+// echo "<pre>";
+// print_r($params);
+// echo "</pre>";
